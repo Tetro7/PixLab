3ebaac82785fb19d056158ac3383992e43439a47
Tetro7 <ilya.konyaev.96@mail.ru>
2022-05-30 22:54:13 +0300

    фича. добавил поворот логотипа по клику на 360 градусов

--- app/app/src/main/java/com/pixlab/app/MainActivity.java
+++ app/app/src/main/java/com/pixlab/app/MainActivity.java
@@ -2,12 +2,24 @@ package com.pixlab.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        logo = findViewById(R.id.logo);
+
+        logo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.animate().rotation(360);
+            }
+        });
     }
 }

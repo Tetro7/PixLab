e26298d03552e9c1e8d9bb3743fe8b0a50eac4f6
Tetro7 <ilya.konyaev.96@mail.ru>
2022-06-07 23:45:27 +0300

    поработали на славу

--- app/app/src/main/java/com/pixlab/app/MainActivity.java
+++ app/app/src/main/java/com/pixlab/app/MainActivity.java
@@ -1,12 +1,24 @@
 package com.pixlab.app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     private ImageView logo;
+    private ConstraintLayout windowCreateCanvas;
+    private Button createButton;
+    private Button windowCloseButton;
+    private Button size32;
+    private Button size64;
+    private Button size128;
+    private EditText inputWidth;
+    private EditText inputHeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,11 +26,57 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         logo = findViewById(R.id.logo);
+        windowCreateCanvas = findViewById(R.id.windowCreateCanvas);
+        createButton = findViewById(R.id.createButton);
+        windowCloseButton = findViewById(R.id.windowCloseButton);
+        size32 = findViewById(R.id.size32);
+        size64 = findViewById(R.id.size64);
+        size128 = findViewById(R.id.size128);
+        inputWidth = findViewById(R.id.inputWidth);
+        inputHeight = findViewById(R.id.inputHeight);
 
         logo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 v.animate().rotationBy(360);
+            }
+        });
+
+        createButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                windowCreateCanvas.setVisibility(View.VISIBLE);
+            }
+        });
+
+        windowCloseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                windowCreateCanvas.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        size32.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                inputWidth.setText("32");
+                inputHeight.setText("32");
+            }
+        });
+
+        size64.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                inputWidth.setText("64");
+                inputHeight.setText("64");
+            }
+        });
+
+        size128.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                inputWidth.setText("128");
+                inputHeight.setText("128");
             }
         });
     }
